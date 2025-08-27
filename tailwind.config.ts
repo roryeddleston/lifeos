8215b5f4daa3b7be84699ccc9252122c42fb924f
Rory Eddleston <rory_94@live.co.uk>
2025-08-27 11:21:14 +0100

    creating tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+export default {
+  darkMode: "class",
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/lib/**/*.{ts,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
