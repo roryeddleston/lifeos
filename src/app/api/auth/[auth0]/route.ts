01a890fde79202e0001f7c3dd30330107bd9daa0
Rory Eddleston <rory_94@live.co.uk>
2025-09-02 18:09:27 +0100

    removing unsed auth0 files

--- src/app/api/auth/[auth0]/route.ts
+++
@@ -1,6 +0,0 @@
-import { handleAuth } from "@auth0/nextjs-auth0/edge";
-
-export const runtime = "edge"; // required when using the edge helpers
-
-export const GET = handleAuth();
-export const POST = handleAuth();
