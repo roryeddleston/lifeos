de70202d9500173fda92c49eaf757f571dd60011
Rory Eddleston <rory_94@live.co.uk>
2025-11-02 20:12:39 +0000

    making prisma reusable in production

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    // 'query' logging is noisy & can cause perf hits; warn/error is enough
-    log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "warn", "error"]
+        : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") {
+if (!globalForPrisma.prisma) {
   globalForPrisma.prisma = prisma;
 }
